560fb9ab974cc47bd2fe67c3e3d98553875e756b
will <william.souza@raroprestador.com.br>
2024-02-10 15:49:35 -0300

    feat: add webservers

--- api/go.mod
+++ api/go.mod
@@ -1,5 +1,8 @@
 module github.com/willsza/ecommerce-go-js/api
 
-go 1.18
+go 1.22.0
 
-require github.com/google/uuid v1.6.0
+require (
+	github.com/go-chi/chi/v5 v5.0.11
+	github.com/google/uuid v1.6.0
+)
---
+++ api/internal/webserver/product_handler.go
@@ -0,0 +1,79 @@
+package webserver
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/willsza/ecommerce-go-js/api/internal/entity"
+	"github.com/willsza/ecommerce-go-js/api/internal/service"
+)
+
+type WebProductHandler struct {
+	ProductService *service.ProductService
+}
+
+func NewWebProductHandler(productService *service.ProductService) *WebProductHandler {
+	return &WebProductHandler{ProductService: productService}
+}
+
+func (wph *WebProductHandler) GetProducts(w http.ResponseWriter, r *http.Request) {
+	products, err := wph.ProductService.GetProducts()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(products)
+}
+
+func (wph *WebProductHandler) GetProduct(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		http.Error(w, "Invalid product id", http.StatusBadRequest)
+		return
+	}
+
+	product, err := wph.ProductService.GetProduct(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(product)
+}
+
+func (wph *WebProductHandler) GetProductsByCategory(w http.ResponseWriter, r *http.Request) {
+	category := chi.URLParam(r, "category")
+	if category == "" {
+		http.Error(w, "Invalid category", http.StatusBadRequest)
+		return
+	}
+
+	products, err := wph.ProductService.GetProductsByCategory(category)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(products)
+}
+
+func (wph *WebProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product entity.Product
+
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	result, err := wph.ProductService.CreateProduct(product.Name, product.Description, product.CategoryID, product.ImageURL, product.Price)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(result)
+}
