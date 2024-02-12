3cf4d6ad276f62e0480cba39eaf6895cd834ab19
will <william.souza@raroprestador.com.br>
2024-02-12 18:21:06 -0300

    chore: add typeorm and mysql

--- orders-api/src/products/entities/product.entity.ts
+++ orders-api/src/products/entities/product.entity.ts
@@ -1,1 +1,18 @@
-export class Product {}
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ type: 'text' })
+  description: string;
+
+  @Column()
+  price: number;
+
+  @Column()
+  image_url: string;
+}
