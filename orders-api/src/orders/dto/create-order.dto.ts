2da540e8728d2a5033b7b7e6e3d7036d28ca73b1
will <william.souza@raroprestador.com.br>
2024-02-12 22:26:49 -0300

    feat: add validations

--- orders-api/src/orders/dto/create-order.dto.ts
+++ orders-api/src/orders/dto/create-order.dto.ts
@@ -1,9 +1,34 @@
+import { Type } from 'class-transformer';
+import {
+  ArrayNotEmpty,
+  IsInt,
+  IsNotEmpty,
+  IsPositive,
+  IsString,
+  MaxLength,
+  ValidateNested,
+} from 'class-validator';
+
 export class CreateOrderDto {
+  @ArrayNotEmpty()
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDto)
   items: OrderItemDto[];
+
+  @MaxLength(255)
+  @IsNotEmpty()
+  @IsString()
   card_hash: string;
 }
 
 export class OrderItemDto {
+  @IsString()
+  @MaxLength(36)
+  @IsNotEmpty()
   product_id: string;
+
+  @IsPositive()
+  @IsInt()
+  @IsNotEmpty()
   quantity: number;
 }
